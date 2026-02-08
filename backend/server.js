9f0a04f6f337b4dcc597265ad3f5c3331c0770ef
mayurpawar1219 <mayur121900@gmail.com>
2026-02-08 13:59:52 +0530

    Refactor server.js for improved structure

--- backend/server.js
+++ backend/server.js
@@ -4,8 +4,8 @@ import { connectDB } from "./config/db.js";
 import foodRouter from "./routes/foodRouter.js";
 import userRouter from "./routes/userRoute.js";
 import cartRouter from "./routes/cartRoute.js";
-import "dotenv/config.js"
 import orderRouter from "./routes/orderRoute.js";
+import "dotenv/config.js";
 
 // App Config
 const app = express();
@@ -15,23 +15,24 @@ const port = process.env.PORT || 4000;
 app.use(express.json());
 app.use(cors());
 
-// Serve static files from "uploads" directory
+// Static files
 app.use("/uploads", express.static("uploads"));
-app.use("/api/user",userRouter);
-app.use("/api/cart",cartRouter);
-app.use("/api/order",orderRouter);
 
-// Database Connection
-connectDB();
-
-// API Endpoints
+// Routes
+app.use("/api/user", userRouter);
+app.use("/api/cart", cartRouter);
+app.use("/api/order", orderRouter);
 app.use("/api/food", foodRouter);
 
+// Health check
 app.get("/", (req, res) => {
   res.send("API Working");
 });
 
+// Database Connection
+connectDB();
+
 // Start Server
 app.listen(port, () => {
-  console.log(`Server is running on port http://localhost:${port}`);
+  console.log(`Server running on port ${port}`);
 });
